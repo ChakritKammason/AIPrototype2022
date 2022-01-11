3b878c67369ee3040de14f601ed3a06c4c9a39a0
ChakritKammason <chakrit_kammason@kkumail.com>
2022-01-11 10:52:23 +0700

    make argparse function

--- python_scrip_101.py
+++ python_scrip_101.py
@@ -1,5 +1,41 @@
+import argparse #สำหรับ รับ input จากภายนอก 
+import subprocess #สำหรับ run terminal command
+
+#import flask #สำหรับ ทำ web app และ web service api
+
+
 def print_other():
     print('something else')
 
+def parse_input():
+
+    if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='test')
+    parser.add_argument(
+        'M',
+        type=int,
+        help='value of M Positional argument')
+
+    parser.add_argument(
+        '--x',
+        type=int,
+        help='value of x')
+
+    parser.add_argument(
+        '--yval',
+        type=int,
+        default=3,
+        help='value of y')
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
-    print('test main function')
+    
+    args = parse_input()
+
+    x = args.x
+    y = args.yval
+    print(f'M = {args.M}')
+    print(f'calculate {x} x {y} = {x*y}')
+
+
