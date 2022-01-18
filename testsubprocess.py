0427825e16fac3b5d178af5064ae75da10b69888
ChakritKammason <chakrit_kammason@kkumail.com>
2022-01-18 09:54:19 +0700

    update

--- testsubprocess.py
+++ testsubprocess.py
@@ -1,10 +1,11 @@
 import subprocess
 
 if __name__ == "__main__":
+    #basic
     #subprocess.run(["ls", "-l"])
     #for i in [2,5,6,8]:
-    #    subprocess.run(["python", "python_scrip_101.py", "9", "--x", f"{i}", "--yval", "2"])
-
+        #subprocess.run(["python", "python_scrip_101.py", "9", "--x", f"{i}", "--yval", "2"])
+    
 
     ##use ouput of subprocess
     #pro = subprocess.Popen(["ls", "-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -13,7 +14,7 @@ if __name__ == "__main__":
 
     #HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูณ
     for i in [2,5,6,8,]:
-        process = subprocess.Popen(["python", "python_scrip_101.py", "9", "--x", f"{i}", "--yval", "2"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = process.communicate()
-        print(str(out))
+        pro = subprocess.Popen(["python", "python_scrip_101.py", "9", "--x", f"{i}", "--yval", "2"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = pro.communicate()
+        print(out)
 
